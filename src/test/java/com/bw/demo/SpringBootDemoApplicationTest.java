8554d3824bb5f2cc9df6d756c5ac449f58871d32
baowei <baow@chinatelecom.cn>
2021-01-18 14:27:08 +0800

    初始化

---
+++ src/test/java/com/bw/demo/SpringBootDemoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.bw.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringBootDemoApplicationTest {
+    @Test
+    public void contextLoad() {
+    }
+}
