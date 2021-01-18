ee640cc3fab39c342b760aef9be5341ef7d37863
baowei <baow@chinatelecom.cn>
2021-01-18 15:09:31 +0800

    线程池和定时任务demo

--- src/main/java/com/bw/demo/SpringBootDemoApplication.java
+++ src/main/java/com/bw/demo/SpringBootDemoApplication.java
@@ -2,7 +2,9 @@ package com.bw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootDemoApplication {
     public static void main(String[] args) {
