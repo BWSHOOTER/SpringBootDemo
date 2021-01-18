2f7699caf080da17f6799833d61202f27c8191a7
BWSHOOTER <bwebsite@163.com>
2021-01-18 15:59:22 +0800

    Revert "Merge pull request #1 from BWSHOOTER/Executor_&_Scheduled"
    This reverts commit 2c6ac4a712b2eb32eddc72ca3e1eb2247df8be10, reversing
    changes made to 5f7810c9d0c962ca3ced677140de96882a2b941a.

--- src/main/java/com/bw/demo/SpringBootDemoApplication.java
+++ src/main/java/com/bw/demo/SpringBootDemoApplication.java
@@ -2,9 +2,7 @@ package com.bw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class SpringBootDemoApplication {
     public static void main(String[] args) {
